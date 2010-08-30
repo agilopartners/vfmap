ac58608c54b89f254a2352c71bc518a88500d804
Miika Pihlaja <pihlaja@iki.fi>
2010-08-31 00:37:01 +0300

    initial commit

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+  $dbhost = 'mysql4-ext.sigmatic.fi:3306';
+  $dbuser = 'asuntoag_map';
+  $dbpass = 'asEq143jk';
+  $dbname = 'asuntoag_vuokraforum_production';
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  include 'config.php';
+  include 'opendb.php';
+
+  $query = "SELECT offer_ads.id, offer_ads.room_types, ad_gmaps_locations.lat, ad_gmaps_locations.lng ".
+           "FROM offer_ads, ad_gmaps_locations ".
+           "WHERE offer_ads.id = ad_gmaps_locations.offer_ad_id ".
+  	       "LIMIT 10";
+  	       
+  $result = mysql_query($query) or die(mysql_error());
+  
+  while($row = mysql_fetch_assoc($result))
+  {
+      echo "id:{$row['id']} " .
+           "room_types: {$row['room_types']} ".
+           "lat:{$row['lat']} " .
+           "lng:{$row['lng']} <br />";
+  } 
+  
+
+  include 'closedb.php';
+?>
